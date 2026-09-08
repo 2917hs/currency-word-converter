a115729f3db6b1c2e5e2959b8c9f66e6900aa0d3
Hasan Siddiqui <hasan190889@gmail.com>
2026-09-08 14:39:34 +0200

    setup thin client for different providers (languages)

--- CurrencyWordConverter/CurrencyToWordConverter.Core/Enumerations/Language.cs
+++ CurrencyWordConverter/CurrencyToWordConverter.Core/Enumerations/Language.cs
@@ -5,3 +5,31 @@ public enum Language
     English,
     German
 }
+
+public static class LanguageCodes
+{
+    private const string English = "en";
+
+    private const string German =    "de";
+
+    public static readonly IReadOnlyDictionary<string, Language> CodeToLanguage =
+        new Dictionary<string, Language>(StringComparer.OrdinalIgnoreCase)
+        {
+            [English] = Language.English,
+            [German] = Language.German
+        };
+
+    public static readonly IReadOnlyDictionary<Language, string> LanguageToCode =
+        new Dictionary<Language, string>
+        {
+            [Language.English] = English,
+            [Language.German] = German
+        };
+
+    public static readonly IReadOnlyDictionary<Language, string> LanguageToDisplayName =
+        new Dictionary<Language, string>
+        {
+            [Language.English] = "English",
+            [Language.German] = "Deutsch (German)"
+        };
+}
